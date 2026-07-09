2690c705eab5d96fea171b2565d9a164c1b27c14
Vijay-Apple <vijayraj639726@gmail.com>
2026-07-09 17:52:59 +0000

    completed

--- src/public/controllers/contactController.js
+++ src/public/controllers/contactController.js
@@ -1,33 +1,69 @@
-import Contact from "../models/Contact.js";
+import nodemailer from "nodemailer"
+import dotenv from "dotenv"
+dotenv.config();
 
 export const createContact = async (req, res) => {
-    try {
-        const { name, email, subject, message } = req.body;
+  try {
+    const { name, email, subject, message } = req.body;
 
-        if (!name || !email || !subject || !message) {
-            return res.status(400).json({
-                success: false,
-                message: "All fields are required",
-            });
-        }
-
-        const contact = await Contact.create({
-            name,
-            email,
-            subject,
-            message,
-        });
-
-        return res.status(201).json({
-            success: true,
-            message: "Message sent successfully",
-            data: contact,
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            success: false,
-            message: "Internal Server Error",
-        });
+    if (!name || !email || !subject || !message) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
-};
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD
+      }
+    })
+    const mailOptions = {
+      from: process.env.SMTP_EMAIL,
+      to: process.env.CONTACT_RECEIVER_EMAIL,
+      subject: `new contact form submission - ${subject}`,
+      html: `
+        <h2>New Contact Form Submission</h2>
+
+        <table border="1" cellpadding="10" cellspacing="0">
+          <tr>
+             <td><strong>Full Name</strong></td>
+             <td>${name}</td>
+          </tr>
+
+          <tr>
+            <td><strong>Email</strong></td>
+            <td>${email}</td>
+          </tr>
+          <tr>
+            <td><strong>Subject</strong></td>
+            <td>${subject}</td>
+          </tr>
+
+          <tr>
+            <td><strong>Message</strong></td>
+            <td>${message}</td>
+          </tr>
+        </table>
+      `
+    }
+    await transporter.sendMail(mailOptions);
+
+    return res.status(200).json({
+      success: true,
+      message: "Message sent successFully"
+    })
+
+  } catch (err) {
+    console.log("ERROR : " + err)
+    res.status(500).json({
+      success: false,
+      message: "Failed to send message"
+    })
+
+  }
+}
